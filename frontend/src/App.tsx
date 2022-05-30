6fd54430a5e666b3e5810651332abef53145d538
Kaka <kakalition@gmail.com>
2022-05-30 19:47:20 +0700

    Create scss/custom.scss for customizing Bootstrap; Import output of sass, bootstrap.css to main.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import { Button } from 'react-bootstrap';
-import './App.css';
 
 export default function App() {
   return (
     <div className="full-screen">
       <h1>kadsjnfksadbfasbf</h1>
       <Button variant="primary">Test Primary</Button>
+      <Button variant="danger">Test Primary</Button>
     </div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import './App.css';
+import './bootstrap.css';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
