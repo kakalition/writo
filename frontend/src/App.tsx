111b98877173d6256a1f70bd004a48f8a0766d53
Kaka <kakalition@gmail.com>
2022-05-30 19:28:22 +0700

    Modify App.css full-screen class

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 export default function App() {
   return (
     <div className="full-screen">
       <h1>kadsjnfksadbfasbf</h1>
+      <Button variant="primary">Test Primary</Button>
     </div>
   );
 }
