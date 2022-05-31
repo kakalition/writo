8bc2bb3db4f6aefd17c0e683697253eebed77e3f
Kaka <kakalition@gmail.com>
2022-05-31 19:11:21 +0700

    Create database migrations for Writo

---
+++ backend/app/Http/Controllers/NoteTagController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NoteTag;
+use Illuminate\Http\Request;
+
+class NoteTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NoteTag  $noteTag
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NoteTag $noteTag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\NoteTag  $noteTag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, NoteTag $noteTag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NoteTag  $noteTag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NoteTag $noteTag)
+    {
+        //
+    }
+}
