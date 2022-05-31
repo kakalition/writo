bdd1cff7b2273f5c5040b4ae9f8483cbbd9ff21c
Kaka <kakalition@gmail.com>
2022-05-31 12:31:22 +0700

    Create SearchbarComponent

--- frontend/src/features/app/note-list/NoteListPage.tsx
+++ frontend/src/features/app/note-list/NoteListPage.tsx
@@ -1,6 +1,8 @@
+import { Form, FormGroup } from 'react-bootstrap';
 import { createUseStyles } from 'react-jss';
 import GridIcon from '../../../common-component/icons/GridIcon';
 import PlusIcon from '../../../common-component/icons/PlusIcon';
+import SearchIcon from '../../../common-component/icons/SearchIcon';
 import TagIcon from '../../../common-component/icons/TagIcon';
 import UserIcon from '../../../common-component/icons/UserIcon';
 import { BackgroundStyles, FontStyles, SpacerStyles } from '../../../common-component/JSSUtilities';
@@ -18,6 +20,13 @@ const NoteListStyles = createUseStyles({
     strokeWidth: '2px',
     margin: '0.5rem',
   },
+  searchIconClass: {
+    width: '2rem',
+    height: '2rem',
+    color: 'gray',
+    strokeWidth: '2px',
+    margin: '0.5rem',
+  },
   ctaIconClass: {
     display: 'flex',
     alignItems: 'center',
@@ -30,6 +39,25 @@ const NoteListStyles = createUseStyles({
     borderRadius: '0.8rem',
     color: 'black',
     strokeWidth: '0.1rem',
+  },
+  searchbarClass: {
+    padding: '1rem 3rem 1rem 3rem',
+    borderTop: '1px solid transparent',
+    borderBottom: '1px solid gray',
+  },
+  searchbarInputClass: {
+    width: '100%',
+    height: '100%',
+    color: 'gray',
+    fontSize: '1.5rem',
+    outlineColor: 'transparent',
+    borderColor: 'transparent',
+    '&:focus': {
+      color: 'gray',
+      outlineColor: 'transparent',
+      borderColor: 'transparent',
+      boxShadow: 'none',
+    },
   },
 });
 
@@ -53,6 +81,32 @@ function AppNavbar() {
   );
 }
 
+function SearchBar() {
+  const noteListStyles = NoteListStyles();
+  const fontStyles = FontStyles();
+  const spacerStyles = SpacerStyles();
+  const backgroundStyles = BackgroundStyles();
+
+  return (
+    <div id="searchbar" className={`d-flex flex-row w-100 align-items-center justify-content-start ${noteListStyles.searchbarClass}`}>
+      <div className={`${noteListStyles.searchIconClass}`}><SearchIcon /></div>
+      <div style={{ width: '1rem' }} />
+      <div className="w-100 h-100">
+        <Form className="h-100">
+          <Form.Group controlId="search" className="h-100">
+            <Form.Control
+              className={`${fontStyles.roboto} ${noteListStyles.searchbarInputClass} h-100`}
+              id="search-query"
+              name="search-query"
+              placeholder="Covalent Bonding"
+            />
+          </Form.Group>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
 export default function NoteListPage() {
   const noteListStyles = NoteListStyles();
   const fontStyles = FontStyles();
@@ -62,6 +116,9 @@ export default function NoteListPage() {
   return (
     <div id="page-container" className="d-flex flex-row vh-100 vw-100">
       <AppNavbar />
+      <div id="app-content" className="d-flex flex-column w-100 h-100">
+        <SearchBar />
+      </div>
     </div>
   );
 }
