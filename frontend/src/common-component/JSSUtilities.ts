b50f03dc4ab1ee4ee16e5441a5ac607dca4d0f3f
Kaka <kakalition@gmail.com>
2022-05-31 07:24:46 +0700

    Add UndrawBookSvg component; Finish HomePage layout

---
+++ frontend/src/common-component/JSSUtilities.ts
@@ -0,0 +1,10 @@
+import { createUseStyles } from 'react-jss';
+
+export const SpacerStyles = createUseStyles({
+  h2: {
+    height: '2rem',
+  },
+  h4: {
+    height: '4rem',
+  },
+});
--- frontend/src/features/homepage/HomePage.tsx
+++ frontend/src/features/homepage/HomePage.tsx
@@ -1,5 +1,7 @@
 import { Button } from 'react-bootstrap';
 import { createUseStyles } from 'react-jss';
+import { SpacerStyles } from '../../common-component/JSSUtilities';
+import UndrawBookSvg from '../../common-component/UndrawBookSvg';
 
 const HomeStyles = createUseStyles({
   pageClass: {
@@ -28,16 +30,11 @@ const HomeStyles = createUseStyles({
     fontSize: '5rem',
     color: 'white',
   },
-  h4: {
-    height: '4rem',
-  },
-  h8: {
-    height: '8rem',
-  },
 });
 
 export default function HomePage() {
   const homeStyles = HomeStyles();
+  const spacerStyles = SpacerStyles();
 
   return (
     <div className={`d-flex flex-column vh-100 vw-100 bg-dark ${homeStyles.pageClass}`}>
@@ -45,9 +42,9 @@ export default function HomePage() {
         <h1 className={homeStyles.topbarHeadingClass}>Writio</h1>
         <Button className={homeStyles.buttonClass} variant="outline-light">Login</Button>
       </div>
-      <div id="main-content-container" className="d-flex align-items-center justify-content-center w-100 h-100">
+      <div id="main-content-container" className="d-flex flex-column align-items-center justify-content-center w-100 h-100">
         <div id="main-content" className="d-flex align-items-center justify-content-between w-100 h-100 flex-row">
-          <div id="content" className="d-flex flex-column align-items-start justify-content-center w-50 h-100 borderize">
+          <div id="content" className="d-flex flex-column align-items-start justify-content-center w-50">
             <h2 className={homeStyles.contentClass}>
               Taking note
               <br />
@@ -55,12 +52,15 @@ export default function HomePage() {
               <br />
               this fun.
             </h2>
-            <div className={homeStyles.h4} />
+            <div className={spacerStyles.h4} />
             <Button className={homeStyles.ctaClass} variant="light">Get Started</Button>
-            <div className={homeStyles.h4} />
           </div>
-          <div id="illustration" />
+          <div id="illustration" className="d-flex flex-column align-items-center justify-content-center w-50">
+            <UndrawBookSvg />
+            <div className={spacerStyles.h2} />
+          </div>
         </div>
+        <div className={spacerStyles.h4} />
       </div>
     </div>
   );
