21549ad8baeea1759db8dc5520359815d39fa406
Kaka <kakalition@gmail.com>
2022-06-09 14:16:37 +0700

    Create isSmall param in SearchbarComponent

--- frontend/src/features/app/note-list/components/SearchbarComponent.tsx
+++ frontend/src/features/app/note-list/components/SearchbarComponent.tsx
@@ -1,17 +1,27 @@
 import SearchIcon from '../../../../common-component/icons/SearchIcon';
 
-export default function SearchbarComponent() {
-  const iconClass = 'w-10 h-10 stroke-gray-500 stroke-[0.1rem]';
+type Params = {
+  isSmall?: boolean
+};
+
+export default function SearchbarComponent({ isSmall = false }: Params) {
+  const iconClass = `${isSmall ? 'w-6 h-6' : 'w-10 h-10'} stroke-gray-500 stroke-[0.1rem]`;
 
   return (
-    <div id="searchbar" className="flex z-[1] flex-row justify-start items-center py-4 px-12 w-full border-b-2 border-b-gray-200 shadow-md">
+    <div
+      id="searchbar"
+      className={`flex z-[1] flex-row justify-start items-center w-full border-b-2 border-b-gray-200 shadow-md
+      ${isSmall ? 'py-3 px-6' : 'py-4 px-12'}
+      `}
+    >
       <div className={iconClass}><SearchIcon /></div>
       <div className="w-4" />
       <form className="w-full h-full">
         <input
           aria-label="search query"
           id="query"
-          className="w-full h-full text-xl text-gray-500 placeholder:text-gray-500 focus:outline-0"
+          className={`w-full h-full text-gray-500 placeholder:text-gray-500 bg-transparent focus:outline-0
+          ${isSmall ? 'text-base' : 'text-xl'}`}
           type="text"
           name="query"
           placeholder="Covalent bonding"
--- frontend/src/features/app/note-page/NotePage.tsx
+++ frontend/src/features/app/note-page/NotePage.tsx
@@ -1,3 +1,12 @@
+import SearchbarComponent from '../note-list/components/SearchbarComponent';
+
 export default function NotePage() {
-  return <div>Reserved</div>;
+  return (
+    <div className="w-full h-full">
+      <div className="flex flex-col w-[20%] h-full bg-gray-50">
+        <SearchbarComponent isSmall />
+      </div>
+      <div className="w-[80%] h-full bg-white" />
+    </div>
+  );
 }
