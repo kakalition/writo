7b04177d0ee2929e7034562cfe1cedac2e21af40
Kaka <kakalition@gmail.com>
2022-06-14 08:45:14 +0700

    Add general note tests

--- backend/tests/Feature/NoteTagsTest.php
+++ backend/tests/Feature/NoteTagsTest.php
@@ -5,6 +5,7 @@ use function Pest\Laravel\getJson;
 use function Pest\Laravel\postJson;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
 
 uses(RefreshDatabase::class);
 
@@ -100,4 +101,84 @@ test('should return 204 when successfully delete note tag.', function () {
   ]);
 
   $request->assertNoContent();
+});
+
+/****************
+ * GENERAL TEST *
+ ****************/
+
+test('should return correct note data when creating and deleting note tag.', function () {
+  login_user('k@K', '00000000');
+
+  $response_one = getJson('api/users/k@k/notes/note-title');
+  $response_one->assertOk();
+  $response_one->assertJson(function (AssertableJson $json) {
+    return $json->where('title', 'Note Title')
+      ->where('body', 'Note Body')
+      ->where('tags', [])
+      ->etc();
+  });
+
+  create_note_tag_on_user_email('k@k', 'note-title', 'test-tag');
+  $response_two = getJson('api/users/k@k/notes/note-title');
+  $response_two->assertOk();
+  $response_two->assertJson(function (AssertableJson $json) {
+    return $json->where('title', 'Note Title')
+      ->where('body', 'Note Body')
+      ->has('tags', 1)
+      ->has(
+        'tags.0',
+        fn ($json) =>
+        $json->where('name', 'Test Tag')
+          ->where('background_color', '#000000')
+          ->where('text_color', '#FFFFFF')
+          ->etc()
+      )
+      ->etc();
+  });
+
+  create_tag_on_user_email('k@k', 'Tech', '#000000', '#FFFFFF');
+  create_note_tag_on_user_email('k@k', 'note-title', 'tech');
+  $response_three = getJson('api/users/k@k/notes/note-title');
+  $response_three->assertOk();
+  $response_three->assertJson(function (AssertableJson $json) {
+    return $json->where('title', 'Note Title')
+      ->where('body', 'Note Body')
+      ->has('tags', 2)
+      ->has(
+        'tags.0',
+        fn ($json) =>
+        $json->where('name', 'Test Tag')
+          ->where('background_color', '#000000')
+          ->where('text_color', '#FFFFFF')
+          ->etc()
+      )
+      ->has(
+        'tags.1',
+        fn ($json) =>
+        $json->where('name', 'Tech')
+          ->where('background_color', '#000000')
+          ->where('text_color', '#FFFFFF')
+          ->etc()
+      )
+      ->etc();
+  });
+
+  deleteJson('api/users/k@k/note-tags', ['note_title' => 'Note Title', 'tag_name' => 'Test Tag']);
+  $response_four = getJson('api/users/k@k/notes/note-title');
+  $response_four->assertOk();
+  $response_four->assertJson(function (AssertableJson $json) {
+    return $json->where('title', 'Note Title')
+      ->where('body', 'Note Body')
+      ->has('tags', 1)
+      ->has(
+        'tags.0',
+        fn ($json) =>
+        $json->where('name', 'Tech')
+          ->where('background_color', '#000000')
+          ->where('text_color', '#FFFFFF')
+          ->etc()
+      )
+      ->etc();
+  });
 });
